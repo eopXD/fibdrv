ba2de05f28f80f878b03fe1f2ada95ee132ab19f
eopXD <yueh.ting.chen@gmail.com>
2020-07-18 00:04:48 +0800

    naive implementation for uint128_t

---
+++ uint128_XD.h
@@ -0,0 +1,88 @@
+// uint128 implemented by 2 unsigned long long
+
+typedef struct uint128 {
+	unsigned long long lo, hi;
+} uint128_XD;
+const uint128_XD zero128 = {
+	.hi = 0,
+	.lo = 0
+};
+// static makes functions restricted to the file where they are declared
+static inline void add ( uint128_XD *ret, uint128_XD x, uint128_XD y ) {
+	ret->hi = x.hi + y.hi;
+	if ( y.lo > ~x.lo ) {
+		ret->hi++;
+	}
+	ret->lo = x.lo + y.lo;
+}
+
+// since they are unsigned...
+static inline void sub ( uint128_XD *ret, uint128_XD x, uint128_XD y ) {
+	ret->hi = x.hi - y.hi;
+	if ( x.lo < y.lo ) {
+		ret->hi--;
+	} 
+	ret->lo = x.lo - y.lo;		
+}
+
+static inline void lsft ( uint128_XD *ret, uint128_XD x, int shift ) {
+	if ( shift == 0 ) {
+		*ret = x;
+		return ;
+	}
+	if ( shift >= 64 ) {
+		ret->hi = x.lo << (shift - 64);
+		ret->lo = 0ull;
+		return ;
+	}
+	ret->hi = x.hi << shift;
+	ret->hi |= x.lo >> (64-shift);
+	ret->lo = x.lo << shift;
+}
+
+static inline void rsft ( uint128_XD *ret, uint128_XD x, int shift ) {
+	if ( shift == 0 ) {
+		*ret = x;
+		return ;
+	}
+	if ( shift >= 64 ) {
+		ret->lo = x.hi >> (shift - 64);
+		ret->hi = 0ull;
+		return ;
+	}
+	ret->lo = x.lo >> shift;
+	ret->lo |= x.hi << (64-shift);
+	ret->hi = x.hi >> shift;
+}
+// check if the i-th bit of x is 1 (0-th bit is LSB)
+static inline int is_one ( uint128_XD x, int i ) {
+	if ( i >= 128 ) {
+		exit(1);
+	}
+	if ( i >=64 ) {
+		return (x.hi >> (i-64)) & 1;
+	}
+	else {
+		return (x.lo >> i) & 1;
+	}
+}
+static inline void mul ( uint128_XD *ret, uint128_XD x, uint128_XD y ) {
+	*ret = zero128;
+	int a = __builtin_popcountll(x.lo) + __builtin_popcountll(x.hi);
+	int b = __builtin_popcountll(y.lo) + __builtin_popcountll(y.hi);
+	uint128_XD l, u; // u = Multiplicand, l = Multiplier
+	if ( a >= b ) {
+		u = x;
+		l = y;
+	} else {
+		u = y;
+		l = x;
+	}
+	for ( int i=0; i<128; ++i ) {
+		if ( is_one(l, i) ) {
+			uint128_XD tmp;
+			lsft(&tmp, u, i);
+			add(ret, *ret, tmp);
+		}
+	}
+}
---
+++ uint128_XD_test.c
@@ -0,0 +1,48 @@
+// tests uint128_XD with <stdint.h>'s __int128
+#include <stdio.h>
+#include <limits.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <time.h>
+
+#include "uint128_XD.h"
+
+#define NINETEEN 10000000000000000000ull
+typedef unsigned long long ull;
+typedef unsigned __int128 uint128_t;
+ull randull () {
+	return (ull)rand() * (ull)rand();
+}
+void print128_t ( uint128_t q ) {
+	ull hi, lo;
+	hi = q / NINETEEN;
+	lo = q % NINETEEN;
+	if ( hi == 0ull ) {
+		printf("%llu\n", lo);
+	}
+	else {
+		printf("%llu%019llu\n", hi, lo);
+	}
+}
+uint128_t XD_to_std ( uint128_XD y ) {
+	uint128_t x = y.hi;
+	x = (x << 64) + y.lo;
+	return x;
+}
+void print128_XD ( uint128_XD q ) {
+	print128_t(XD_to_std(q));
+}
+int main ()
+{
+	srand(time(NULL));
+	int t = 100;
+	for ( int i=0; i<t; ++i ) {
+		uint128_t a = randull(), b = randull();
+		uint128_XD x = zero128, y = zero128, xy;
+		x.lo = a, y.lo = b;
+		mul(&xy, x, y);
+		print128_t(a*b);
+		print128_XD(xy);
+	}
+	return 0;
+}
